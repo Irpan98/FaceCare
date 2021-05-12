d823476fcc3a282506e105eeb75d48a7f1a36d2d
Irpan <irpant1998@gmail.com>
2021-05-12 18:39:37 +0800

    main page

--- app/src/main/java/id/itborneo/facecare/main/MainActivity.kt
+++ app/src/main/java/id/itborneo/facecare/main/MainActivity.kt
@@ -1,14 +1,33 @@
 package id.itborneo.facecare.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import id.itborneo.facecare.R
+import id.itborneo.facecare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        //update from master
+        initBinding()
+        initBottomNav()
+    }
+
+    private fun initBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    private fun initBottomNav() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bottomNav, navController)
     }
 }
---
+++ app/src/main/java/id/itborneo/facecare/result/ResultActivity.kt
@@ -0,0 +1,12 @@
+package id.itborneo.facecare.result
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.itborneo.facecare.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
